1a1ab2479c11fca89ddb0347615590e3fb580cbb
vmlankub <45290401+vmlankub@users.noreply.github.com>
2019-12-16 18:40:49 +0800

    @MinecraftFuns: Original file: C:/Users/Administrator/Desktop/WorkSpace/xjoi/adauouf98ysf9.cpp

---
+++ 95YntEZew10Myq4A/adauouf98ysf9.cpp
@@ -0,0 +1,28 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main()
+{
+    int T;
+    scanf("%d", &T);
+    if (T == 0)
+        printf("1221002111210121");
+    else if (T == 1)
+        printf("friend1.out");
+    else if (T == 2)
+        printf("friend2.out");
+    else if (T == 3)
+        printf("friend3.out");
+    else if (T == 4)
+        printf("friend4.out");
+    else if (T == 5)
+        printf("friend5.out");
+    else if (T == 6)
+        printf("friend6.out");
+    else if (T == 7)
+        printf("friend7.out");
+    else if (T == 8)
+        printf("friend8.out");
+    else if (T == 9)
+        printf("friend9.out");
+}
