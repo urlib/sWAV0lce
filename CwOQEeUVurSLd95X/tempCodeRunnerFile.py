fbf691be56270fd8ed1465ff29588125930815a5
vmlankub <45290401+vmlankub@users.noreply.github.com>
2019-12-16 15:20:43 +0800

    @MinecraftFuns: Original file: C:/Users/Administrator/Desktop/WorkSpace/lg/tempCodeRunnerFile.py

---
+++ CwOQEeUVurSLd95X/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+m, n = int(input()), int(input())
+# l, r = 0, 1
+# while r ** m <= n:
+#     l = r
+#     r = r * 2
+# ans = 0
+# while l <= r:
+#     mid = (l + r) // 2
+#     if mid ** m <= n:
+#         ans = mid
+#         l = mid + 1
+#     else:
+#         r = mid - 1
+# print(ans)
